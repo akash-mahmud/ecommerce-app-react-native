5ca709fcbf0644f489fa29380ed677d630808233
Akash Mahmud <mahmudakash177@gmail.com>
2022-10-17 21:34:58 +0600

    ui has completed

---
+++ App.tsx
@@ -0,0 +1,23 @@
+import "react-native-gesture-handler";
+import React from "react";
+import {SafeAreaView, View, StatusBar, useColorScheme} from "react-native";
+
+import {Colors} from "react-native/Libraries/NewAppScreen";
+import Router from "./src/router";
+
+const App = () => {
+  const isDarkMode = useColorScheme() === "dark";
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    flex: 1,
+  };
+  return (
+    <View style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? "light-content" : "dark-content"} />
+      <Router />
+    </View>
+  );
+};
+
+export default App;
